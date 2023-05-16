aad1d28f324e430fb5f6a5d9c074a2e902f58fb8
Steele Shreve <shrevesa@mail.uc.edu>
2023-05-16 16:08:47 -0400

    Implement live-reload with Vite for development purposes

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      cssnext: false,
-    }),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('postcss-url')({
-      url: 'rebase',
-    }),
     require('cssnano')({
       preset: 'default',
     }),
-  ]
-}
+  ],
+};
