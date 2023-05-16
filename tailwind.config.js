221fd72a64b13f7c60f426ac7a247c4780c6321a
Steele Shreve <shrevesa@mail.uc.edu>
2023-05-15 21:12:35 -0400

    Add custom classes for font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./*.{html,js}"],
   darkMode: 'media',
   theme: {
+    fontFamily: {
+      display: ['Inter Bold'],
+      sans: ['Inter Regular'],
+      monospace: ['Inconsolata'],
+    },
     extend: {},
   },
   plugins: [],
